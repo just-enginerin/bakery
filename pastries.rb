a7d0ae3bcd08219a8e2651bd8724dede0ae417b1
Erin Brady <ebrady@nyc.yearup.org>
2019-12-13 12:18:24 -0500

    created Cookie, Muffin, and Cake sub classes

--- pastries.rb
+++ pastries.rb
@@ -7,3 +7,30 @@ class Pastry
         @image = image
     end
 end
+
+class Cookie < Pastry
+    attr_accessor :name, :type, :description, :price, :vegan, :image
+    def initialize(name, description, vegan = false, image)
+        super(name, description, vegan, image)
+        @type = "Cookie"
+        @price = 3.99
+    end
+end
+
+class Muffin < Pastry
+    attr_accessor :name, :type, :description, :price, :vegan, :image
+    def initialize(name, description, vegan = false, image)
+        super(name, description, vegan, image)
+        @type = "Muffin"
+        @price = 4.99
+    end
+end
+
+class Cake < Pastry
+    attr_accessor :name, :type, :description, :price, :vegan, :image
+    def initialize(name, description, vegan = false, image)
+        super(name, description, vegan, image)
+        @type = "Cake"
+        @price = 5.99
+    end
+end
