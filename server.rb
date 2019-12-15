059cc7c1425110efdb2ed148db5dd8678d14d3e7
itsanerin <31420768+itsanerin@users.noreply.github.com>
2019-12-14 20:52:33 -0500

    set up homepage and file structure

--- server.rb
+++ server.rb
@@ -2,21 +2,25 @@ require 'sinatra'
 require './pastries.rb'
 
 get '/' do
-
   erb :index
 end
 
-get '/cakes' do
+get '/about' do
+  erb :about
+end
 
+get '/cakes' do
   erb :cakes
 end
 
 get '/cookies' do
-
   erb :cookies
 end
 
 get '/muffins' do
-
   erb :muffins
+end
+
+get '/visit' do
+  erb "visit"
 end
