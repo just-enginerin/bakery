789884c33574987ae42f2d655564e95f67dbd6df
itsanerin <31420768+itsanerin@users.noreply.github.com>
2020-01-17 09:56:53 -0500

    pause twilio and add Gemfile.lock

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require './pastries.rb'
-require 'twilio-ruby'
+# require 'twilio-ruby'
 
 enable :sessions
 
@@ -9,23 +9,23 @@ get '/' do
   erb :index
 end
 
-post '/' do
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
-    client = Twilio::REST::Client.new(account_sid, auth_token)
+# post '/' do
+#     account_sid = ENV['TWILIO_ACCOUNT_SID']
+#     auth_token = ENV['TWILIO_AUTH_TOKEN']
+#     client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    from = ENV['TWILIO_PHONE_NUM']
-    to = params['user_phone_num']
+#     from = ENV['TWILIO_PHONE_NUM']
+#     to = params['user_phone_num']
 
-    client.messages.create(
-    from: from,
-    to: to,
-    body: "Hey there! Congrats on signing up for discounts at Dough Re Mi. Tell the cashier that your favorite song is Birthday Cake by Rihanna in order to receive a 10% discount off your purchase. Enjoy!")
+#     client.messages.create(
+#     from: from,
+#     to: to,
+#     body: "Hey there! Congrats on signing up for discounts at Dough Re Mi. Tell the cashier that your favorite song is Birthday Cake by Rihanna in order to receive a 10% discount off your purchase. Enjoy!")
 
-    session[:message] = "Successfully sent an SMS!"
+#     session[:message] = "Successfully sent an SMS!"
 
-    erb :home
-end
+#     erb :home
+# end
 
 get '/about' do
   erb :about
