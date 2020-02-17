a80f9f3c4b01e5443056432ca5aa6cef8d062b32
itsanerin <31420768+itsanerin@users.noreply.github.com>
2020-02-17 14:23:39 -0500

    added twilio

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require './pastries.rb'
-# require 'twilio-ruby'
+require 'twilio-ruby'
 
 enable :sessions
 
@@ -10,19 +10,19 @@ get '/' do
 end
 
 post '/' do
-#     account_sid = ENV['TWILIO_ACCOUNT_SID']
-#     auth_token = ENV['TWILIO_AUTH_TOKEN']
-#     client = Twilio::REST::Client.new(account_sid, auth_token)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new(account_sid, auth_token)
 
-#     from = ENV['TWILIO_PHONE_NUM']
-#     to = params['user_phone_num']
+    from = ENV['TWILIO_PHONE_NUM']
+    to = params['user_phone_num']
 
-#     client.messages.create(
-#     from: from,
-#     to: to,
-#     body: "Hey there! Congrats on signing up for discounts at Dough Re Mi. Tell the cashier that your favorite song is Birthday Cake by Rihanna in order to receive a 10% discount off your purchase. Enjoy!")
+    client.messages.create(
+    from: from,
+    to: to,
+    body: "Hey there! Congrats on signing up for discounts at Dough Re Mi. Tell the cashier that your favorite song is Birthday Cake by Rihanna in order to receive a 10% discount off your purchase. Enjoy!")
 
-#     session[:message] = "Successfully sent an SMS!"
+    session[:message] = "Successfully sent an SMS!"
 
     erb :home
 end
